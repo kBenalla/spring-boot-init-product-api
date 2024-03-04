8790b2ed1fc4a144a628e796637ab8aafb4d8197
Khalil Benalla <khalilbenalla097@gmail.com>
2024-03-04 17:40:16 +0100

    Spring Boot initiation product-api

---
+++ src/test/java/org/com/productapi/ProductapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
